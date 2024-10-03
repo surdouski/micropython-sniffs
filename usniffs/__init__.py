ecf7c71d73802a8f309fca44f64e3f011820df38
surdouski <michael.surdouski@gmail.com>
2024-10-03 15:42:32 -0400

    add on_connect and on_disconnect hooks

--- usniffs/__init__.py
+++ usniffs/__init__.py
@@ -7,10 +7,12 @@ from usniffs.router import Router
 class Sniffs:
     """A dynamic wrapper for the mqtt_as client (mqtt_as wrote by Peter Hinch)."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, on_connect=None, on_disconnect=None, *args, **kwargs):
         self.client = None
         self._awaitable_returns = AwaitableReturns()
         self.router = Router(self._awaitable_returns)
+        self.on_connect = on_connect
+        self.on_disconnect = on_disconnect
 
     async def bind(self, client):
         self.client = client
@@ -19,7 +21,7 @@ class Sniffs:
         asyncio.create_task(self._messages())
         asyncio.create_task(self._down())
 
-        await asyncio.sleep(10)  # maybe this is not a great strategy; revisit this
+        await asyncio.sleep(0)
 
     def route(self, topic_route: str):
         """A decorator for adding route registration."""
@@ -35,16 +37,23 @@ class Sniffs:
         while True:
             await self.client.up.wait()
             self.client.up.clear()
+
             print("We are connected to broker.")
             paths = self.router.get_topic_paths()
             for path in paths:
                 await self.client.subscribe(path)
+
+            if self.on_connect:
+                await self.on_connect()
 
     async def _down(self):
         while True:
             await self.client.down.wait()  # Pause until outage
             self.client.down.clear()
             print("WiFi or broker is down.")
+
+            if self.on_disconnect:
+                await self.on_disconnect()
 
     async def _messages(self):
         async for topic, msg, retained in self.client.queue:
